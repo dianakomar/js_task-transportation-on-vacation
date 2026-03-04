e32332738ebd06d5d312d2701918ce0d8902a378
Diana Komarchuk <dianakocmarchuk2508@gmail.com>
2026-03-04 21:17:48 +0200

    Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,24 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const numberOfDays = days;
+  const PRICE = 40;
+
+  const LONG_TERM = 7;
+  const MEDIUM_TERM = 3;
+
+  const LONG_TERM_DISCOUNT = 50;
+  const MEDIUM_TERM_DISCOUNT = 20;
+
+  const basePrice = numberOfDays * PRICE;
+
+  if (numberOfDays >= LONG_TERM) {
+    return basePrice - LONG_TERM_DISCOUNT;
+  } else if (numberOfDays >= MEDIUM_TERM) {
+    return basePrice - MEDIUM_TERM_DISCOUNT;
+  }
+
+  return numberOfDays * PRICE;
 }
 
 module.exports = calculateRentalCost;
